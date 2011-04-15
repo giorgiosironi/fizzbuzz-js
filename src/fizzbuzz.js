1f9a2aa510d1457bdf5b0b4c37a4faeea9fc6822
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-04-15 15:56:32 +0200

    First test, with configuration

---
+++ src/fizzbuzz.js
@@ -0,0 +1,4 @@
+function fizzbuzz()
+{
+    return 'Fizz';
+}
---
+++ test/fizzbuzz.js
@@ -0,0 +1,5 @@
+TestCase("FizzBuzzTest", {
+    "test should return Fizz when passed 3" : function () {
+        assertEquals("Fizz", fizzbuzz(3));
+    }
+});
