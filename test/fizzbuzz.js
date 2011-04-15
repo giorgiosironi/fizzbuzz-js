0beb1549e8613d7a68eb37ce60d3289cde836f0a
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-04-15 16:01:24 +0200

    Added regression test

--- test/fizzbuzz.js
+++ test/fizzbuzz.js
@@ -3,7 +3,8 @@ TestCase("FizzBuzzTest", {
         assertEquals(1, fizzbuzz(1));
         assertEquals(2, fizzbuzz(2));
     },
-    "test should return Fizz when passed 3" : function () {
+    "test should return Fizz when passed 3 or a multiple" : function () {
         assertEquals("Fizz", fizzbuzz(3));
-    }
+        assertEquals("Fizz", fizzbuzz(6));
+    },
 });
